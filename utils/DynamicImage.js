0f84b5381da0dd0862785cd6fe2a17018de926e9
YoussefNesafe <ynessafe@gmail.com>
2023-04-14 02:00:32 +0400

    fix: daynamic image

--- utils/DynamicImage.js
+++ utils/DynamicImage.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 
 const DynamicImages = ({ file, params = "original/", ...rest }) => {
 	const imageBaseURL = "https://image.tmdb.org/t/p/";
 	const imgUrl = `${imageBaseURL}${params}${file}`;
-	return <Image src={imgUrl} alt="" {...rest} />;
+	return <img src={imgUrl} alt="" {...rest} />;
 };
 
 export default DynamicImages;
