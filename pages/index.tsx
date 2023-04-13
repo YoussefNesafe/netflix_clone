8b96ffe1bfe4a859a6cc750d38fba14f803b3a08
YoussefNesafe <ynessafe@gmail.com>
2023-04-14 02:13:46 +0400

    fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home = ({
 		<div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
 			<Head>
 				<title>Home - Netflix</title>
-				<link rel="icon" href="/public/favicon.png" />
+				<link rel="icon" href="/favicon.png" />
 			</Head>
 			<Navbar />
 			<main className="relative pb-24 pl-4 lg:space-y-24 lg:pl-16 ">
--- utils/DynamicImage.js
+++ utils/DynamicImage.js
@@ -1,9 +1,9 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 
-const DynamicImage = ({ file, params = "original/", ...rest }) => {
-	const imageBaseURL = "https://image.tmdb.org/t/p/";
+const DynamicImages = ({ file, params = "original/", ...rest }) => {
+	const imageBaseURL = process.env.NEXT_PUBLIC_MOVIE_IMAGE_BASE_URL;
 	const imgUrl = `${imageBaseURL}${params}${file}`;
-	return <img src={imgUrl} alt="" {...rest} />;
+	return <Image src={imgUrl} alt="" {...rest} />;
 };
 
-export default DynamicImage;
+export default DynamicImages;
