d8a958d5848df89a32d3a0b86b94a9f694cd090b
YoussefNesafe <ynessafe@gmail.com>
2023-04-13 05:05:45 +0400

    add: app props context hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import AppPropsContext from "@/context/appPropsContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<AppPropsContext.Provider value={pageProps}>
+			<Component {...pageProps} />
+		</AppPropsContext.Provider>
+	);
 }
