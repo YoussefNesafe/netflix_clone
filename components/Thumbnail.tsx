df439dbea7915dd09beb3e49d2365f30a10fd103
YoussefNesafe <ynessafe@gmail.com>
2023-04-14 02:03:39 +0400

    fix: daynamic image

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Movie } from "../typings";
-import DynamicImages from "@/utils/DynamicImage";
+import DynamicImage from "@/utils/DynamicImage";
 
 interface Props {
 	movie: Movie;
@@ -11,7 +11,7 @@ function Thumbnail({ movie }: Props) {
 		<div
 			className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
 		>
-			<DynamicImages
+			<DynamicImage
 				params="w500"
 				file={movie.backdrop_path || movie.poster_path}
 				className="object-cover rounded-sm md:rounded"
--- utils/DynamicImage.js
+++ utils/DynamicImage.js
@@ -1,9 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 
-const DynamicImages = ({ file, params = "original/", ...rest }) => {
+const DynamicImage = ({ file, params = "original/", ...rest }) => {
 	const imageBaseURL = "https://image.tmdb.org/t/p/";
 	const imgUrl = `${imageBaseURL}${params}${file}`;
 	return <img src={imgUrl} alt="" {...rest} />;
 };
 
-export default DynamicImages;
+export default DynamicImage;
