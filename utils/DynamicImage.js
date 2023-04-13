c038d72dc8a85be65faf2810dfc3da563571f3ea
YoussefNesafe <ynessafe@gmail.com>
2023-04-14 01:53:05 +0400

    fix: image production error

--- utils/DynamicImage.js
+++ utils/DynamicImage.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const DynamicImages = ({ file, params = "original/", ...rest }) => {
-	const imageBaseURL = process.env.NEXT_PUBLIC_MOVIE_IMAGE_BASE_URL;
+	const imageBaseURL = "https://image.tmdb.org/t/p/";
 	const imgUrl = `${imageBaseURL}${params}${file}`;
 	return <Image src={imgUrl} alt="" {...rest} />;
 };
